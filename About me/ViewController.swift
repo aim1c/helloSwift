a36c03539ab7255aeb15ad65d9e8df5a89d3307c
Alexander Volkov <buttonpanicph@gmail.com>
2019-10-27 14:51:48 +0300

    Initial Commit

---
+++ About me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Alexander Volkov on 27/10/2019.
+//  Copyright © 2019 Alexander Volkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
