4cb8b130b31993251122d84a405ee0a71e317283
Alexander Volkov <buttonpanicph@gmail.com>
2019-10-27 15:43:16 +0300

    Simple test

--- About me/ViewController.swift
+++ About me/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		let label = UILabel()
+		let text = "Hello, I'm Alexander"
+		label.text = text
+		
+		let font = UIFont(name: "Futura-Bold", size: 24)
+		label.font = font
+		
+		let size = label.intrinsicContentSize
+		let findFrame = {(size: CGSize) -> CGRect in
+			let x = self.view.frame.midX - size.width / 2
+			let y = self.view.frame.midY - size.width / 2
+			let newFrame = CGRect(x: x, y: y, width: size.width, height: size.height)
+			
+			return newFrame
+		}
+		
+		let frame = findFrame(size)
+		
+		label.frame = frame
+		self.view.addSubview(label)
 	}
 
 
